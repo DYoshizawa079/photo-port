bd8a570fd18db7702b547f24c7dc0e7349d231ec
DYoshizawa079 <daniely079@gmail.com>
2022-03-10 00:58:42 -0500

    Added contact form markup

---
+++ src/components/PhotoList/index.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+//import Modal from '../Modal';
+
+function PhotoList({ category }) {
+
+    const [photos] = useState([
+        {
+            name: 'Grocery aisle',
+            category: 'commercial',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Grocery booth',
+            category: 'commercial',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Building exterior',
+            category: 'commercial',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Restaurant table',
+            category: 'commercial',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Cafe interior',
+            category: 'commercial',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Cat green eyes',
+            category: 'portraits',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Green parrot',
+            category: 'portraits',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Yellow macaw',
+            category: 'portraits',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Pug smile',
+            category: 'portraits',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Pancakes',
+            category: 'food',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Burrito',
+            category: 'food',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Scallop pasta',
+            category: 'food',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Burger',
+            category: 'food',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Fruit bowl',
+            category: 'food',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Green river',
+            category: 'landscape',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Docks',
+            category: 'landscape',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Panoramic village by sea',
+            category: 'landscape',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Domestic landscape',
+            category: 'landscape',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+        {
+            name: 'Park bench',
+            category: 'landscape',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+        },
+    ]);
+    /*  filter() is used to return array items that only meet a particular criteria.
+        Items are returned as an array.
+        In this case, returns only photos that fit a certain category.
+     */
+    const currentPhotos = photos.filter((photo) => photo.category === category);
+
+    return (
+        <div>
+            <div className="flex-row">
+                {/* 
+                    Process each item in the currentPhotos array with the map function
+                */}
+                {currentPhotos.map((image, index) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${index}.jpg`)}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        // 'key' attribute must be there. Otherwise it'll cause an error
+                        key={image.name}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default PhotoList;
